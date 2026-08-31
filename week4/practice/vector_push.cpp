ad1d8bff589918cf4158cdd953504443a34d14f6
mimitrix123 <317410895+mimitrix123@users.noreply.github.com>
2026-08-31 22:41:33 +0530

    Add Week 4 vector push practice

---
+++ week4/practice/vector_push.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){vector<int>v;for(int i=0;i<5;++i){int x;cin>>x;v.push_back(x);}cout<<"Vector elements: ";for(int x:v)cout<<x<<' ';cout<<'\n';}
